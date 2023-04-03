1577cc869fe969701d7c7ea040c1363d5f2eff71
TenNM <pacs3131@gmail.com>
2023-04-03 21:40:00 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace R6S_MidGameLagFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryFix();
+
+            Console.ReadKey();
+        }
+
+        static void TryFix()
+        {
+            Process[] processes = Process.GetProcessesByName("RainbowSix");
+            if (processes.Length == 0)
+            {
+                LogEr("Processes not found");
+                return;
+            }
+
+            var r6 = processes[0];
+            if (null == r6)
+            {
+                LogEr("Process not found");
+                return;
+            }
+
+            IntPtr h = r6.Handle;
+
+            UIntPtr oneCoreMask = (UIntPtr)(0x0001);
+            UIntPtr yourCoreMask;
+
+            if (GetProcessAffinityMask(h, out yourCoreMask, out _))
+            {
+                LogOk("Get affinity");
+            }
+            else
+            {
+                LogEr("Cant get affinity");
+                return;
+            }
+
+            if (SetProcessAffinityMask(h, oneCoreMask))
+            {
+                LogOk("Affinity set core 0");
+            }
+            else
+            {
+                LogEr("Cant set affinity core 0");
+            }
+
+            if (SetProcessAffinityMask(h, yourCoreMask))
+            {
+                LogOk("Affinity restored\nSuccess");
+            }
+            else
+            {
+                LogEr("Cant restore affinity, restore it yourself with task manager");
+            }
+        }
+
+        static void LogOk(string msg) => COut(true, msg);
+        static void LogEr(string msg) => COut(false, msg);
+
+        static void COut(bool res, string msg)
+        {
+            if (res)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("OK: " + msg);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ER: " + msg);
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetProcessAffinityMask(IntPtr hProcess, out UIntPtr lpProcessAffinityMask, out UIntPtr lpSystemAffinityMask);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetProcessAffinityMask(IntPtr hProcess, UIntPtr processAffinityMask);
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+﻿# What is it?
+Strange way to fix rainbow six siege mid game freezes
+## FAQ
+- Why does it work?
+- I don't know, but it is useful for me
+
+- System requirements
+- Windows | .NET Framework 4.5 | less than 64 cores cpu (virtual cores, like hyper-threading)
+
+- Does this affect game files or other data?
+- No, only game process affinity (https://en.wikipedia.org/wiki/Processor_affinity)
+
+Also, you can set affinity manual with task manager. But my program can do it instantly.
+## How to use?
+0. Configure fix program .exe to run as administrator (once)
+1. Play as usual
+2. After mid game freezes run fix exe
+3. Play as usual
+## Features:
+- mid game freezes fix
+- output affinity information to the console
+### See functional in [Form1](https://github.com/TenNM/R6S_MidGameLagFix/blob/master/Program.cs)
